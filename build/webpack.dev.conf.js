3372ff78c555e3cc7a3b672888795b178fda3612
gumm <13121182705@163.com>
2018-08-21 16:37:31 +0800

    four

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -17,13 +17,10 @@ const express = require('express')
 const app = express() //请求server
 var appData = require('../data.json') //加载本地数据文件
 var seller = appData.seller //获取对应的本地数据
-var goods = addData.goods;
+var goods = appData.goods;
 var ratings = appData.ratings
 var apiRoutes = express.Router()
 app.use('/api', apiRoutes)
-
-
-
 
 
 const devWebpackConfig = merge(baseWebpackConfig, {
@@ -64,27 +61,24 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     before(app) {
       app.get('/api/seller', (req, res) => {
           res.json({
-            erron: 0,
+            errno: 0,
             data: seller
           }) //接口返回json数据，上面配置的数据seller就赋值给data请求后使用
         }),
         app.get('/api/goods', (req, res) => {
           res.json({
-            erron: 0,
+            errno: 0,
             data: goods
           })
         }),
         app.get('/api/ratings', (req, res) => {
           res.json({
-            erron: 0,
+            errno: 0,
             data: ratings
           })
         })
     }
 
-
-
-    
 
   },
   plugins: [
